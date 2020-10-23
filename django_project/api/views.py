9fd9c0f434bdbd9bf93842cee89d9a158af85ea6
SuperAlina2020 <alinagithub@gmail.com>
2020-10-23 18:57:18 +0600

    generate secret views

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import random
 
 # Create your views here.
+
+def generate_secret():
+    x = random.sample([1,2,3,4,5,6,7,8,9,0],4)
+    return x
+
+def count_bulls(answer,guess):
+    bulls = 0
+    i = 0
+    while i < len(guess):
+        if guess[i] == answer[i]:
+            bulls = bulls + 1
+        i = i + 1
+    return bulls
+
+
+def count_cows(answer,guess):
+    cows = 0
+    i = 0
+    while i < len(guess):
+        if i in answer:
+            cows = cows + 1
+        i = i + 1
+    return cows
+
+def result(request):
+    answer = generate_secret()
+    guess = [1,2,3,4]
+    bulls = count_bulls(answer,guess)
+    cows = count_cows(answer,guess)
+    return render(request,'api/home.html',{'bulls':bulls,'cows':cows})
+
